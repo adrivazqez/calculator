7589ed6ee41090d863a5dcb48184a07d49158557
Adri Vázquez <adridune@proton.me>
2024-07-06 19:18:49 +0200

    adding functionalities to more buttons

--- calculus.js
+++ calculus.js
@@ -1,44 +1,65 @@
-// Creo la variable que obtendrá el número que aparece en la pantalla de la calculadora
+// Variable que obtendrá el número que aparece en la pantalla de la calculadora la primera vez
 let register = document.querySelector(".result")
 let elementsInRegister = Array.from(register);
 
-console.log(register.innertext);
 
 const buttonsFourthRow = document.querySelector(".fourth-row");
 const buttonsThirdRow = document.querySelector(".third-row");
 const buttonsSecondRow = document.querySelector(".second-row");
-
+const zeroButton = document.getElementById("zero");
 
 
 // addEventListener para los botones que añaden números a la calculadora
-
 buttonsFourthRow.addEventListener("click", function (event){
     elementsInRegister.push(`${event.target.innerText}`)
     register.innerText = elementsInRegister.join("");
 })
 
 buttonsThirdRow.addEventListener("click", function (event) {
-    console.log(`You clicked on button number ${event.target.innerText}`)
+    elementsInRegister.push(`${event.target.innerText}`)
+    register.innerText = elementsInRegister.join("");
 })
 
 buttonsSecondRow.addEventListener("click", function(event){
-    console.log(`You clicked on button number ${event.target.innerText}`)
+    elementsInRegister.push(`${event.target.innerText}`)
+    register.innerText = elementsInRegister.join("");
 })
 
-console.log(elementsInRegister)
+zeroButton.addEventListener("click", function (event){
+    elementsInRegister.push(`${event.target.innerText}`)
+    register.innerText = elementsInRegister.join("");
+})
 
 
+// Funcionalidad "clear" del botón C
+const deleteButton = document.getElementById("C");
+
+deleteButton.addEventListener("click", function () {
+    elementsInRegister.splice(0, elementsInRegister.length)
+    register.innerText = "0";
+})
 
 
+// Variable que almacenará el primer número de la operación introducido por el usuario
+let storage = 0
+let secondNumber = 0
 
 
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
 
+// addEventListener para las funciones de operación -- WIP
+plusButton.addEventListener("click", function () {
+    parseInt(register.innerText);
+    storage = register.innerText;
 
+    elementsInRegister.splice(0, elementsInRegister.length)
+    register.innerText = "0"
 
-
-
-
-
+    console.log(storage);
+})
 
 
 
